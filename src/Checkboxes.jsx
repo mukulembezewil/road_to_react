612b1d840781a432964ed4c51e3bc4531eda886f
mukulembezewil <wilmukulembeze@gmail.com>
2024-01-11 14:43:02 +0300

    Cleanout Checkboxes component?

--- src/Checkboxes.jsx
+++ src/Checkboxes.jsx
@@ -1,15 +1,60 @@
 import * as React from 'react';
+// import { useState } from 'react';
 
 const Chekbaxez = () => {
+	const [student, setStudent] = React.useState(false);
+	const [marital, setMarital] = React.useState(false);
+	const [developer, setDeveloper] = React.useState(false);
+
+	const handleStudentChange = () => {
+		setStudent(!student);
+	};
+
+	const handleMaritalChange = () => {
+		setMarital(!marital);
+	};
+
+	const handleDeveloperChange = () => {
+		setDeveloper(!developer);
+	};
+
 	return (
 		<>
-			<Checkbox />
+			<hr />
+
+			<Checkbox
+				label="Student"
+				value={student}
+				onChange={handleStudentChange}
+			/>
+
+			<Checkbox
+				label="Married"
+				value={marital}
+				onChange={handleMaritalChange}
+			/>
+
+			<Checkbox
+				label="Developer"
+				value={developer}
+				onChange={handleDeveloperChange}
+			/>
 		</>
 	);
 };
 
-const Checkbox = () => {
-	return <input type="checkbox" />;
+// eslint-disable-next-line react/prop-types
+const Checkbox = ({ label, value, onChange }) => {
+	return (
+		<>
+			<input
+				type="checkbox"
+				checked={value}
+				onChange={onChange}
+			/>{' '}
+			{label};
+		</>
+	);
 };
 
 export default Chekbaxez;
