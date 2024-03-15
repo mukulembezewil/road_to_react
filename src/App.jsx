5a596cc02a1ae4484e844b61405d5279d2e9a5f1
mukulembezewil <wilmukulembeze@gmail.com>
2024-03-15 09:38:30 +0300

    After a while commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Appee from '../../blackboard';
 
 const initialStories = [
 	{
@@ -57,11 +58,19 @@ const App = () => {
 	const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
 	const [stories, setStories] = React.useState([]);
+	const [isLoading, setIsLoading] = React.useState(false);
+	const [isError, setIsError] = React.useState(false);
 
 	React.useEffect(() => {
-		getAsyncStories().then((result) => {
-			setStories(result.data.stories);
-		});
+		setIsLoading(true);
+
+		getAsyncStories()
+			.then((result) => {
+				setStories(result.data.stories);
+
+				setIsLoading(false);
+			})
+			.catch(() => setIsError(true));
 	}, []);
 
 	const handleRemoveStory = (item) => {
@@ -95,10 +104,18 @@ const App = () => {
 
 			<hr />
 
-			<List
-				list={searchedStories}
-				onRemoveItem={handleRemoveStory}
-			/>
+			{isError && <p>Something went wrong...</p>}
+
+			{isLoading ? (
+				<p>Loading...</p>
+			) : (
+				<List
+					list={searchedStories}
+					onRemoveItem={handleRemoveStory}
+				/>
+			)}
+
+			<Appee />
 		</div>
 	);
 };
